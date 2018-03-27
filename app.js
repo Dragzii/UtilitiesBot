fd49b70a50879f5d6707ffb13bfdeb6f3941f38c
Dragzii <37547804+Dragzii@users.noreply.github.com>
2018-03-26 19:50:57 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const discord = require('discord.js')
+const client = new discord.Client;
+const Settings = require("./settings.json");
+
+client.on('ready', () => {
+	console.log("me me big boy");
+})
+client.on('message', message => {
+	if (message.content === "!ping") {
+		message.reply("pong!");
+	}
+})
+
+client.login(Settings.token);
