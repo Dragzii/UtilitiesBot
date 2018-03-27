1b327ef3eb7c46ed65f9a815e5ab3425a985537b
Dragzii <37547804+Dragzii@users.noreply.github.com>
2018-03-27 12:47:49 -0700

    index

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const discord = require('discord.js')
-const client = new discord.Client;
-const Settings = require("./settings.json");
+const Client = require("./base/Client");
+const config = require("./config");
 
-client.on('ready', () => {
-	console.log("me me big boy");
-})
-client.on('message', message => {
-	if (message.content === "!ping") {
-		message.reply("pong!");
-	}
-})
+// Extenders
+require("./extenders/Message");
+require("./extenders/MessageEmbed");
+require("./extenders/TextChannel");
+require("./extenders/User");
+// require("./extenders/GuildMember");
 
-client.login(Settings.token);
+// Initialize client, attach config to config.json, set command directory to /commands, and set events directory to /events
+// fetchAllMembers: true, disableEveryone: true, disabledEvents: ["USER_UPDATE", "TYPING_START"]
+const client = new Client({ 
+    ...config.clientOptions,
+    config,
+    guild: config.guild,
+    sql: config.credentials.sql
+ });
+
+ client.loadCommands(config.dirs.commands).loadEvents(config.dirs.events);
+
+// Start client with token found in config
+client.start(config.credentials.token);
